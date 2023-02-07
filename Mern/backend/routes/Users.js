0971cc18fc139658722eafb748e71eab2ee31065
jmfp <therealjmfp@gmail.com>
2023-02-06 21:35:00 -0500

    first

--- Mern/backend/routes/Users.js
+++ Mern/backend/routes/Users.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {addUser} = require('../controller/User')
+const {addUser, loginUser} = require('../controller/User')
 
 router.post('/add-user', addUser)
+router.post('/login', loginUser)
 
 module.exports = router
