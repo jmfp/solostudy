683355b4f0dc97a76369b8de04f36fafa6096166
jmfp <therealjmfp@gmail.com>
2023-02-08 01:52:50 -0500

    0.0.0.0.0.1

--- root/frontend/src/components/Header.js
+++ root/frontend/src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {RiDashboard3Fill} from 'react-icons/ri'
 
 export default function Header() {
   return (
     <header className='header-invis'>
         <div className='logo-container'>
-            <Link to='/'>Home</Link>
+            <Link to='/' style={{textDecoration: "none"}}>Home</Link>
         </div>
         <div>
-            <Link to='/dashboard'>Dashboard</Link>
+            <Link to='/dashboard' style={{textDecoration: "none"}}>
+              <RiDashboard3Fill/>
+            </Link>
         </div>
     </header>
   )
