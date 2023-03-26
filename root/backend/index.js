3dea44aa2ab90aec305a7a1c7ecf451015dedc6e
jmfp <therealjmfp@gmail.com>
2023-03-25 21:08:37 -0400

    courses

--- root/backend/index.js
+++ root/backend/index.js
@@ -10,10 +10,10 @@ const connectDB = require('./database/db')
 connectDB()
 
 const app = express()
-app.use(cors())
 //adding middleware for body data
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors())
 
 //app.get('/', (req, res) => { res.send('Hello from Express!')})
 app.use('/api/products', require("./routes/Products"))
